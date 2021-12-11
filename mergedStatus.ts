8811784050179dc614b5966ad242834a6f0f75d8
Alexis Joel Leite Paracampo <alexisleite@live.com>
2021-12-11 00:50:24 -0300

    initial commit

---
+++ mergedStatus.ts
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+
+type StateUpdater<State> = (prevState: State) => Partial<State>;
+
+export function useMergedState<State extends Record<string, any>>(
+  initialState: State,
+  debug = false
+): [State, (newState: Partial<State> | StateUpdater<State>) => void] {
+  const [state, setState] = useState<State>(initialState);
+
+  function mergeState(newState: StateUpdater<State> | Partial<State>) {
+    if (debug) console.log(newState);
+
+    setState((prevState) => {
+      if (typeof newState === 'function') {
+        newState = newState(prevState);
+      }
+      return { ...prevState, ...newState };
+    });
+  }
+
+  return [state, mergeState];
+}
