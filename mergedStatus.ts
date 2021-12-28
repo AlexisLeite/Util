2ecb5636bb841a75c43ddba7d5fe6074149d38f1
Alexis Leite <alexisleite.uy@gmail.com>
2021-12-28 20:54:46 -0300

    Updated merge state

--- mergedStatus.ts
+++ mergedStatus.ts
@@ -1,23 +1,27 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 type StateUpdater<State> = (prevState: State) => Partial<State>;
 
 export function useMergedState<State extends Record<string, any>>(
   initialState: State,
   debug = false
-): [State, (newState: Partial<State> | StateUpdater<State>) => void] {
+): [
+  State,
+  (mergeState: Partial<State> | StateUpdater<State>) => void,
+  (state: State) => void
+] {
   const [state, setState] = useState<State>(initialState);
 
   function mergeState(newState: StateUpdater<State> | Partial<State>) {
     if (debug) console.log(newState);
 
     setState((prevState) => {
-      if (typeof newState === 'function') {
+      if (typeof newState === "function") {
         newState = newState(prevState);
       }
       return { ...prevState, ...newState };
     });
   }
 
-  return [state, mergeState];
+  return [state, mergeState, setState];
 }
